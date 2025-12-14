7f7fe2a19f1c4ba14d292f67551b2017bbc7e847
andrew-tu <75229429+andrew-tu@users.noreply.github.com>
2025-12-14 13:05:10 +0800

    修正images url結構

--- mydata.js
+++ mydata.js
@@ -5,7 +5,7 @@ const restaurants = [
   {
     name: "NAMA Japanese and Seafood Buffet",
     description: "日本料理吃到飽，在Central World商場內，提供新鮮的壽司、刺身、燒烤等多樣選擇，正值周年慶，1999價位特價1499(含稅1763)，平假日都可。入口在後方Centara Grand飯店24樓。",
-    image: "/images/nama.png",
+    image: "images/nama.png",
     website: "https://www.tablecheck.com/en/nama-japanese-and-seafood-buffet-bangkok/reserve/message",
     address: "24th floor Centara Grand and Bangkok Convention Centre, 999/99 Rama I Rd, Khwaeng Pathum Wan, Pathum Wan, Bangkok 10330, Thailand",
     bookingLink: "https://web.hungryhub.com/en/restaurants/nama-japanese-and-seafood-buffet-hungry-hub-bangkok/web",
@@ -13,7 +13,7 @@ const restaurants = [
   {
     name: "Copper Beyond Buffet Gaysorn Amarin",
     description: "Copper Beyond位於Gaysorn Amarin購物中心內，今年主打周末推出米其林主廚設計的料理，值得再訪。有週末限定菜色。",
-    image: "/images/copper.png",
+    image: "images/copper.png",
     website: "https://www.facebook.com/CopperBeyondBuffet",
     address: "Gaysorn Amarin, Unit 3F-s06, 3F-s07 502, Phloen Chit Rd, Lumphini, Pathum Wan, Bangkok 10330, Thailand",
     bookingLink: "extra.html#用手機開啟官方line訂位",
@@ -21,7 +21,7 @@ const restaurants = [
   {
     name: "手 qraft. (The Empire)",
     description: "在EA, Empire Tower(W Hotel難面對街) 55樓的高空餐廳，提供多樣化的國際料理與精緻甜點，推薦在此享用咖啡與甜點，俯瞰曼谷美景。",
-    image: "/images/qraft.jpg",
+    image: "images/qraft.jpg",
     website: "https://www.facebook.com/qraft.oriental",
     address: "Level 55, EA, Empire Tower, 1 S Sathon Rd, Yan Nawa, Sathon, Bangkok 10120, Thailand",
     bookingLink: "extra.html#不用訂位，直接前往，沒位置就算了",
@@ -29,7 +29,7 @@ const restaurants = [
   {
     name: "Starbucks emquartier @ sky garden",
     description: "在EmQuartier購物中心內的星巴克，溫室設計，綠意盎然，適合拍照打卡與放鬆休憩。(蓬蓬站，EMQuartier，A棟5樓sky garden)",
-    image: "/images/starbucks_emquartier.png",
+    image: "images/starbucks_emquartier.png",
     website: "http://www.starbucks.co.th/",
     address: "12 ชั้น 5 Sukhumvit Rd, Khlong Tan Nuea, Watthana, Bangkok 10110, Thailand",
     bookingLink: "extra.html#不用訂位，直接前往，沒位置就算了",
@@ -37,7 +37,7 @@ const restaurants = [
   {
     name: "Kodtalay Seafood RCA",
     description: "在RCA夜生活區開幕的海鮮吃到飽餐廳，提供蒸煮燒烤等多種料理方式，全新推出799泰銖方案，吃到爆滿為止。(但RCA這間有點遠，還是河濱分店比較近)",
-    image: "/images/kotalay_rca.png",
+    image: "images/kotalay_rca.png",
     website: "https://www.facebook.com/people/Kodtalay-Seafood-Buffet-%E6%B3%B0%E6%B5%B7%E9%B2%9C-%E0%B9%82%E0%B8%84%E0%B8%95%E0%B8%A3%E0%B8%97%E0%B8%B0%E0%B9%80%E0%B8%A5%E0%B8%8B%E0%B8%B5%E0%B8%9F%E0%B8%B9%E0%B9%89%E0%B8%94%E0%B8%9A%E0%B8%B8%E0%B8%9F%E0%B9%80%E0%B8%9F%E0%B8%95%E0%B9%8C-RCA-Rama9/61576190977454/?mibextid=wwXIfr&rdid=TcK6CV4FTHM2yIiu&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F16jiaNAeNf%2F%3Fmibextid%3DwwXIfr",
     address: "888 Chaturathit Rd, Bang Kapi, Huai Khwang, Bangkok 10310, Thailand",
     bookingLink: "https://line.me/R/ti/p/@006jxjnp",
